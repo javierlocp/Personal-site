34d42cc7a66556171d0ea2052724a30897080ef0
Javier Lo <1067312+javierlocp@users.noreply.github.com>
2025-06-26 06:30:44 +0900

    Minor layout changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import MainLayout from "./layouts/MainLayout";
 function App() {
   return (
     <MainLayout>
-      <h1>Welcome to my personal site</h1>
-      <p>This is my first React app!</p>
+    <p>You can replace the content inside {"<MainLayout>...</MainLayout>"} later with {"<Home />"}, {"<About />"}, etc.</p>        
     </MainLayout>
   )
 }
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -3,17 +3,19 @@ import Footer from "../components/Footer"
 
 /*
   MainLayout is the default layout for this site.
-  If you're looking to customize a layout, you can add it in ./src/layouts/CustomLayout.jsx
+  If you're looking to add custom layout, you can add it in ./src/layouts/CustomLayout.jsx
 */
 
 const MainLayout = ({ children }) => {
   return (
     <>
-      <Navbar />
-      <main>
-        {children}
-      </main>
-      <Footer />
+      <div className = "container">
+        <Navbar />
+        <main>
+          {children}
+        </main>
+        <Footer />
+      </div>
     </>
   );
 };
