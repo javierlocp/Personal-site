cb2e9a1514edb3468ac68d8173bfdafc061b9835
Javier Lo <1067312+javierlocp@users.noreply.github.com>
2025-06-26 15:47:42 +0900

    modify index and app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import MainLayout from "./layouts/MainLayout";
 function App() {
   return (
     <MainLayout>
-      <h1>You can replace the content inside {"<MainLayout>...</MainLayout>"} later with {"<Home />"}, {"<About />"}, etc.</h1>        
-      <p className="text-neutral-500">This is a text</p>
+      {/* Page Component Can be Rendered here */}
+      <h1 className="mb-4">I'm a multidisciplinary product designer with 14+ years of experience in product, UX, and visual design.</h1>        
+      <p className="text-neutral-400">In the past, I've led high-impact design initiatives across startups and large organizations like Haven1, Agoda, Brankas, Says.com, and Yield App.</p>
     </MainLayout>
   )
 }
