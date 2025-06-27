c99214c47b69392a3eecca1e4acd76a80ae7c623
Javier Lo <1067312+javierlocp@users.noreply.github.com>
2025-06-28 08:27:16 +0900

    Rearrange post readingtime and date, set description max paragraph to 2 using line clamp in Blog page, add placeholder image in blogpost

--- src/pages/Blog/BlogPost.jsx
+++ src/pages/Blog/BlogPost.jsx
@@ -12,26 +12,26 @@ const BlogPost = () => {
 
   return (
     <article>
-      <div className="mb-2 flex flex-col gap-2">
+      <span className="mb-4 inline-block font-mono text-xs text-neutral-400">
+        {new Date(post.date).toLocaleDateString('en-GB', {
+          weekday: 'long',
+          day: 'numeric',
+          month: 'numeric',
+          year: 'numeric',
+        })}
+        {post.readingTime && ` · ${post.readingTime} min read`}
+      </span>
+      <div className="flex flex-col gap-2">
         <h1>{post.title}</h1>
         <p className="text-pretty">{post.description}</p>
       </div>
-      <div className="mt-4 mb-8 text-sm text-neutral-400">
-        <span className="block shrink-0 font-mono text-xs text-neutral-400">
-          {new Date(post.date).toLocaleDateString('en-GB', {
-            weekday: 'long',
-            day: 'numeric',
-            month: 'numeric',
-            year: 'numeric',
-          })}
-          {post.readingTime && ` · ${post.readingTime} min read`}
-        </span>
-
+      <div className="my-12 text-sm text-neutral-400">
+        <img src="https://placehold.co/600x400" alt="" />
         {post.image && <img src={post.image} alt={post.title} className="mb-6 h-auto w-full rounded-md" />}
       </div>
       <div>
         {post.content.map((paragraph, idx) => (
-          <p key={idx} className="mt-4">
+          <p key={idx} className="mt-4 text-neutral-100">
             {paragraph}
           </p>
         ))}
