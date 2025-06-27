f3d69b9d0ebb12df5dae266adeeae017d1d14e5d
Javier Lo <1067312+javierlocp@users.noreply.github.com>
2025-06-27 16:11:30 +0900

    Reduce page comment into 1 line

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,5 +1,4 @@
-// This is called Barrel File, doing this allows me to
-// pull multiple component from one place
+// This is called Barrel File, doing this allows me to pull multiple component from one place
 export { default as Navbar } from "./Navbar";
 export { default as Footer } from "./Footer";
 export { default as ExternalLink } from "./ExternalLink";
