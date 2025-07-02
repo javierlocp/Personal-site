4cf8f99168c8ca1f1ca8a145ed190296224944be
Javier Lo <1067312+javierlocp@users.noreply.github.com>
2025-07-03 08:50:02 +0900

    fix path resolving to blog

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import Home from './pages/Home';
-import Blog from './pages/Blog/Blog';
+import Blog from './pages/blog/Blog';
 import BlogPost from './pages/blog/BlogPost';
 
 function App() {
