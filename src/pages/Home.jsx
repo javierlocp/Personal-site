d375154e278cc1756f610f08afc9788d21641b2b
Javier Lo <1067312+javierlocp@users.noreply.github.com>
2025-07-09 16:57:42 +0700

    Change intro

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ const Home = () => {
   return (
     <>
       <div className="intro mb-22 flex flex-col gap-4">
-        <h1 className="text-pretty">Multidisciplinary product designer with 14+ years of experience designing human-centered digital products.</h1>
+        <h1 className="text-pretty">I'm a multidisciplinary product designer with a strong focus on designing thoughtful, human-centered digital products.</h1>
         <p className="text-pretty">
           I'm currently leading product design at <ExternalLink href="https://haven1.org/">Haven1</ExternalLink>, a permissioned EVM Layer 1 blockchain. Previously, I led high-impact design
           initiatives and helped build design teams and processes across both startups and large organizations — including <ExternalLink href="https://agoda.comg/">Agoda</ExternalLink>,{' '}
